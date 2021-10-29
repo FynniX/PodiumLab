ac3bca5892c9f80a5e3bb9c24dc7ed4c5e75887d
Fynn Haupt <snoxyhd@gmail.com>
2021-10-29 04:58:21 +0200

    Fresh Restart

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from "./router";
+import './styles/index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
